a7d9ae45847b26b82cb97ec8dd99f421c16fc398
sebastian lunghitano <seba_88_@hotmail.com>
2023-04-03 10:53:46 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Navbar from "./components/Navbar"
+
+function App() {
+  
+
+  return (
+  <>
+    <Navbar/> 
+    <div><h1>Proyecto Store</h1></div>
+    
+   </> )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./navbar.css"
+
+const Navbar = () => {
+  return (
+    <nav className="navList">
+      <p>Home</p>
+      <p>Catalogo</p>
+      <p>Contacto</p>
+    </nav>
+
+    
+  );
+};
+
+export default Navbar
