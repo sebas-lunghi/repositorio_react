668931f6ca2e5e8f35b1bf9df110f9aab2a6f94d
sebastian lunghitano <seba_88_@hotmail.com>
2023-05-10 01:48:43 -0300

    quinto commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Navbar from "./components/Navbar"
 
 function App() {
 
+  const [productos, setProductos] = useState([]);
+  
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+    .then((res)=> res.json())
+    .then((res)=> setProductos(res))
+    }, []);
+    
+    console.log(productos);
+  
 
   return (
   <>
