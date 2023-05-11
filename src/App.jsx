5a840497c1286b7ba977dcf1ae16ba2a2059c7b5
sebastian lunghitano <seba_88_@hotmail.com>
2023-05-11 02:55:33 -0300

    commit pre firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,38 @@
-
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Navbar from "./components/Navbar"
+import { useEffect, useState } from 'react';
+import './App.css';
+import Navbar from "./components/Navbar";
+import reactLogo from './assets/react.svg';
 
 function App() {
-
   const [productos, setProductos] = useState([]);
-  
+
+  const getProductos = async () => {
+    const res = await fetch("https://fakestoreapi.com/products");
+    const data = await res.json();
+    setProductos(data);
+  };
+
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-    .then((res)=> res.json())
-    .then((res)=> setProductos(res))
-    }, []);
-    
-    console.log(productos);
-  
+    getProductos();
+  }, []);
+
+  console.log(productos);
 
   return (
-  <>
-    <Navbar/> 
-    <div><h1>Proyecto Store</h1></div>
-    
-   </> )
+    <>
+      <Navbar />
+      <div>
+        <h1>Proyecto Store</h1>
+      </div>
+      {productos.map((producto) => (
+        <div key={producto.id}>
+          <h2 style={{color:"black"}}>{producto.title}  </h2>
+          <img src={producto.image} alt="" width={100} height={100} />
+          <h4>{producto.description}</h4>
+        </div>
+      ))}
+    </>
+  );
 }
 
-export default App
+export default App;
